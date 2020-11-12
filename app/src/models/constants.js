f8e6ba32540ce9d9577da21992bf9aae209d191b
Vijay Marupudi <vijay@vijaymarupudi.com>
2020-11-12 17:40:00 -0500

    Axes and removed scrollbars

--- app/src/models/constants.js
+++ app/src/models/constants.js
@@ -1,2 +1,4 @@
-export const HEIGHT = window.innerHeight;
-export const WIDTH = window.innerWidth;
+export const HEIGHT = window.innerHeight - 10;
+export const WIDTH = window.innerWidth - 10;
+// export const HEIGHT = window.innerHeight;
+// export const WIDTH = window.innerWidth;
--- app/src/views/time-view.js
+++ app/src/views/time-view.js
@@ -1,6 +1,10 @@
 import * as d3 from "d3";
 import { HEIGHT, WIDTH } from "../models/constants";
 
+const PADDING_BOTTOM = 40;
+
+const PADDING_LEFT = 40;
+
 /**
  * TimeView object
  */
@@ -13,25 +17,58 @@ export class TimeView {
     this.allData = allData;
     this.viewWidth = WIDTH / 4;
     this.viewHeight = HEIGHT / 3;
+
+    this.mainViewWidth = this.viewWidth - PADDING_LEFT;
+    this.mainViewHeight = this.viewHeight - PADDING_BOTTOM;
+
     this.timeG = svg
       .append("g")
       .classed("time", true)
       .attr("transform", `translate(0, ${this.viewHeight})`);
+
     this.xScale = d3
       .scaleLinear()
-      .domain(d3.extent(allData, (d) => d.year))
-      .range([0, this.viewWidth]);
+      .domain(d3.extent(allData, d => d.year))
+      .range([0, this.mainViewWidth]);
 
     const bins = d3
       .bin()
-      .value((d) => d.year)
+      .value(d => d.year)
       .domain(this.xScale.domain())
       .thresholds(this.xScale.ticks(20))(allData); // Bin the data
 
     this.yScale = d3
       .scaleLinear()
-      .domain([0, d3.max(bins.map((x) => x.length))])
-      .range([0, this.viewHeight]);
+      .domain([0, d3.max(bins.map(x => x.length))])
+      .range([0, this.mainViewHeight]);
+
+
+    const graphedYScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(bins.map(x => x.length))])
+      .range([this.mainViewHeight, 0]);
+
+    this.timeG
+      .append("g")
+      .attr("style", "font-size: 8px;")
+      .attr("transform", `translate(${PADDING_LEFT - 10}, 0)`)
+      .call(d3.axisLeft(graphedYScale));
+
+  
+    this.timeG
+      .append("text")
+      .attr('text-anchor', 'middle')
+      .attr('font-size', 10)
+      .attr("x", (this.mainViewWidth / 2) + PADDING_LEFT)
+      .attr("y", this.viewHeight - 5)
+      .text("Year")
+
+
+    this.timeG
+      .append("g")
+      .attr("style", "font-size: 8px;")
+      .attr("transform", `translate(${PADDING_LEFT}, ${this.mainViewHeight + 10})`)
+      .call(d3.axisBottom(this.xScale));
   }
 
   /**
@@ -50,7 +87,7 @@ export class TimeView {
           const bounds = [self.xScale.invert(x1), self.xScale.invert(x2)];
           self.filterBounds = bounds;
           onFilter(
-            (d) =>
+            d =>
               d.year >= self.filterBounds[0] && d.year <= self.filterBounds[1]
           );
           break;
@@ -66,10 +103,7 @@ export class TimeView {
     // create the brush
     const brush = d3
       .brushX()
-      .extent([
-        [0, 0],
-        [this.viewWidth, this.viewHeight],
-      ])
+      .extent([[0, 0], [this.mainViewWidth, this.mainViewHeight]])
       .on("start brush end", onBrush);
 
     // add the brush to the svg
@@ -77,7 +111,12 @@ export class TimeView {
       .append("g")
       .classed("brush", true)
       .call(brush)
-      .attr("transform", `translate(0, ${this.viewHeight})`);
+      .attr("transform", `translate(${PADDING_LEFT}, ${this.viewHeight})`);
+
+    this.histG = this.timeG
+      .append("g")
+      .classed("histogram", true)
+      .attr("transform", `translate(${PADDING_LEFT}, 0)`);
 
     self.update(data);
   }
@@ -86,18 +125,18 @@ export class TimeView {
    * Updates the view using filtered data
    */
   update(data) {
-    const x = (d) => this.xScale(d.x0);
+    const x = d => this.xScale(d.x0);
     const maxHeight = this.yScale.range()[1];
-    const binHeight = (d) => this.yScale(d.length);
-    const y = (d) => maxHeight - binHeight(d);
-    const width = (d) => this.xScale(d.x1) - this.xScale(d.x0);
+    const binHeight = d => this.yScale(d.length);
+    const y = d => maxHeight - binHeight(d);
+    const width = d => this.xScale(d.x1) - this.xScale(d.x0);
     const bins = d3
       .bin()
-      .value((d) => d.year)
+      .value(d => d.year)
       .domain(this.xScale.domain())
       .thresholds(this.xScale.ticks(20))(data);
 
-    this.timeG
+    this.histG
       .selectAll("rect")
       .data(bins)
       .join("rect")
